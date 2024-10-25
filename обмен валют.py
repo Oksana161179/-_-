8275ba1e51bbcfc8f9358ec0b6390705a05f8245
Оксана <oksana.fomenko.161979@gmail.com>
2024-10-25 13:51:32 +0600

    Создана функция exchange для получения из интернета курсов валют

--- обмен валют.py
+++ обмен валют.py
@@ -1,9 +1,28 @@
-from cProfile import label
-
 import requests
 import json
 from tkinter import *
 from tkinter import messagebox as mb
+
+
+def exchange():#создаем функцию обмена
+    code = entry.get()#переменная-code получает-get информацию из поля ввода-entry, которую туда ввели
+
+    if code:#делаем проверку, если поле ввода заполнено
+        try:#делаем обработку исключений
+            response = requests.get('https://open.er-api.com/v6/latest/USD')#получаем ответ-response из своего запроса-requests
+            response.raise_for_status()#проверяем на ошибки
+            data = response.json()#в переменную-data складываем ответ.json в виде словаря
+            if code in data['rates']:#проверяем, существует ли в списке валюта, которую ввел пользователь
+                exchange_rate = data['rates'][code]#в переменную курс обмена-exchange_rate мы положим:
+                # из нашей даты-data выбираем словарь-rates и из него выбираем значение по ключу.
+                # а ключом является code, т.е. те три буквы валюты, который ввел пользователь
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate} {code} за 1 доллар")#выдаем курс в окне информации,
+            else:#если же код валют введен неверно или не найден
+                mb.showerror("Ошибка!", f"Валюта {code} не найдена!")#выводим информацию в окне об ошибке
+        except Exception as e:#обрабатываем исключения
+            mb.showerror("Ошибка!", f"Произошла ошибка: {e}.")#выводим в окне сообщение об ошибке
+    else:#если же пользователь ничего не ввел в поле ввода
+        mb.showwarning("Внимание!", "Введите код валюты!")#предупреждаем его об этом
 
 window = Tk()#создаем окно
 window.title("Курсы обмена валют")#задаем заголовок окну
